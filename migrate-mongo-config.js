49b3de870ca4e0b44dae46da4f90b7ac3409d1b6
Alamin Shaikh <hello@octib.com>
2024-01-03 15:39:06 +0600

    Migrate mongo config added

---
+++ migrate-mongo-config.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv');
+
+// ENV config
+dotenv.config();
+
+// Migrate mongo config
+const config = {
+  mongodb: {
+    url: process.env.MONGO_URI,
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  },
+  useFileHash: false,
+  moduleSystem: 'commonjs',
+  migrationsDir: 'migrations',
+  migrationFileExtension: '.js',
+  changelogCollectionName: 'changelog',
+};
+
+module.exports = config;
