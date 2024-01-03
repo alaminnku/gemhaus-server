2b87372d46c9b9c347a27b0e160ce1781ebe0da9
Alamin Shaikh <hello@octib.com>
2024-01-03 15:38:24 +0600

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import express from 'express';
+import 'express-async-errors';
+const xssClean = require('xss-clean');
+import cookieParser from 'cookie-parser';
+import mongoSanitize from 'express-mongo-sanitize';
+
+// Config
+dotenv.config();
+
+// Port
+const PORT = process.env.PORT || 5100;
+
+// Connect to database
+
+// App
+const app = express();
+
+// Middleware
+app.use(helmet());
+app.use(cookieParser());
+app.use(mongoSanitize());
+app.use(xssClean());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  })
+);
+
+// Routes
+
+// Error middleware - Put after all main routes
+app.use(error);
+
+// Run the server
+app.listen(PORT, () => console.log('Server started'));
